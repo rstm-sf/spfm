2673fae745f1c46abdcc46978a08fa48e5443578
rstm-sf <Rustam Sayfutdinov>
2017-12-06 12:07:47 +0300

    Разбил на две формы. Пересмотрел интерфейс и функциональность.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 
 import main_window as mw
+import start_window as sw
 
 
 from decimal import Decimal
@@ -72,7 +73,7 @@ class Model_CRR(object):
     def increment_n(self):
         self._n += 1
 
-    def calc_Sn(self, rho):
+    def calc_Sn_prev(self, rho):
         self._Sn_prev *= Decimal(1.0 + rho)
 
     def calc_gamma_n(self):
@@ -91,74 +92,91 @@ class Model_CRR(object):
                   F(Sn_prev * Decimal(1.0 + a), k))
         )
 
+    def calc_capital(self):
+        return Decimal(self._beta_n) * self._B0 +\
+            Decimal(self._gamma_n) * self._S0
 
-class Form(QMainWindow, mw.Ui_MainWindow):
+
+class Start_Form(QMainWindow, sw.Ui_StartWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.stack_str = list()
-        self.stack_dict = dict()
-        self._is_connect_up_down = False
         self.test_input()
-        self.pushButton_3.clicked.connect(self.get_choice_history)
-        self.pushButton_4.clicked.connect(self.clear)
         self.pushButton_5.clicked.connect(self.show_dialog)
-
-    def button_up(self):
-        a = self.a
-        self.push_state_to_stack()
-        a.increment_n()
-        a.calc_Sn(a._b)
-        self.update_beta_gamma()
-        self.check_end()
-
-    def button_down(self):
-        a = self.a
-        self.push_state_to_stack()
-        a.increment_n()
-        a.calc_Sn(a._a)
-        self.update_beta_gamma()
-        self.check_end()
-
-    def update_beta_gamma(self):
-        self.a.calc_gamma_n()
-        self.a.calc_beta_n()
-        self.update_beta_gamma_line_edit()
-
-    def update_beta_gamma_line_edit(self):
-        a = self.a
-        self.lineEdit_11.setText(str(a._n))
-        self.lineEdit_10.setText(str(round(a._beta_n, 4)))
-        self.lineEdit_9.setText(str(round(a._gamma_n, 4)))
+        self.pushButton_6.clicked.connect(self.button_change_a)
+        self.pushButton_7.clicked.connect(self.button_change_b)
+        self.pushButton_8.clicked.connect(self.button_change_r)
+        self.pushButton_9.clicked.connect(self.button_change_B0)
+        self.pushButton_10.clicked.connect(self.button_change_S0)
+        self.pushButton_11.clicked.connect(self.button_change_K)
+        self.pushButton_12.clicked.connect(self.button_change_N)
+        self.pushButton_4.clicked.connect(self.calculate)
+        self.main_window = None
 
     def test_input(self):
         ''' (beta, gamma) = (-30, 1 / 3)
         Ширяев А. Н. Основы стохастической финансовой математики'''
         self.a = Model_CRR(0, -2 / 5, 1 / 5, 1, 150, 150, 1)
-        a = self.a
-        a.calc_gamma_n()
-        a.calc_beta_n()
-        self.settext_lineedit_a_b_r_B0_S0_K_N_CN()
-        self.update_beta_gamma()
-        self.check_start()
+        self.settext_lineedit_a_b_r_B0_S0_K_N()
 
     def show_dialog(self):
-        r = self.input_dialog_get_r_a_b("r")
-        a = self.input_dialog_get_r_a_b("a", -1.0, r)
-        b = self.input_dialog_get_r_a_b("b", r, 1.0)
-        B0 = self.input_dialog_get_B0()
-        S0 = self.input_dialog_get_S0_K("S0")
-        K = self.input_dialog_get_S0_K("K")
-        N = self.input_dialog_get_N()
+        r, ok = self.input_dialog_get_r_a_b("r")
+        if not ok:
+            return
+        a, ok = self.input_dialog_get_r_a_b("a", -1.0, r)
+        if not ok:
+            return
+        b, ok = self.input_dialog_get_S0_K_b("b", r)
+        if not ok:
+            return
+        B0, ok = self.input_dialog_get_B0()
+        if not ok:
+            return
+        S0, ok = self.input_dialog_get_S0_K_b("S0")
+        if not ok:
+            return
+        K, ok = self.input_dialog_get_S0_K_b("K")
+        if not ok:
+            return
+        N, ok = self.input_dialog_get_N()
+        if not ok:
+            return
         self.a = Model_CRR(r, a, b, B0, S0, K, N)
-        a = self.a
-        a.calc_gamma_n()
-        a.calc_beta_n()
-        self.settext_lineedit_a_b_r_B0_S0_K_N_CN()
-        self.update_beta_gamma()
-        self.check_start()
+        self.settext_lineedit_a_b_r_B0_S0_K_N()
 
-    def settext_lineedit_a_b_r_B0_S0_K_N_CN(self):
+    def input_dialog_get_r_a_b(self, x: str, l=-1.0, h=1.0):
+        num, ok = QInputDialog.getDouble(
+            self, "Окно ввода", "Введите значение " + x + ":", min=l, max=h
+        )
+        if l < num < h:
+            return num, ok
+        else:
+            raise Exception("Неверно введено " + x + "!")
+
+    def input_dialog_get_S0_K_b(self, x: str, l=0.0):
+        return QInputDialog.getDouble(
+            self, "Окно ввода", "Введите значение " + x + ":", min=l
+        )
+
+    def input_dialog_get_B0(self):
+        num, ok = QInputDialog.getDouble(
+            self, "Окно ввода", "Введите значение B0:", min=0.0
+        )
+        if num > 0.0:
+            return num, ok
+        else:
+            raise Exception("Неверно введено B0!")
+
+    def input_dialog_get_N(self):
+        num, ok = QInputDialog.getInt(
+            self, "Окно ввода", "Введите значение N:", min=1
+        )
+        if num > 0:
+            return num, ok
+        else:
+            raise Exception("Неверно введено N!")
+
+    def settext_lineedit_a_b_r_B0_S0_K_N(self):
         a = self.a
         self.lineEdit.setText(str(a._a))
         self.lineEdit_2.setText(str(a._b))
@@ -167,56 +185,128 @@ class Form(QMainWindow, mw.Ui_MainWindow):
         self.lineEdit_5.setText(str(a._S0))
         self.lineEdit_6.setText(str(a._K))
         self.lineEdit_7.setText(str(a._N))
-        self.lineEdit_8.setText(str(round(float(a._CN), 4)))
 
-    def input_dialog_get_r_a_b(self, x: str, l=-1.0, h=1.0):
-        num, ok = QInputDialog.getDouble(
-            self, "Окно ввода", "Введите значение " + x + ":", min=l, max=h
-        )
-        if ok and l < num < h:
-            return num
-        else:
-            raise Exception("Неверно введено " + x + "!")
+    def button_change_r(self):
+        r, ok = self.input_dialog_get_r_a_b("r")
+        if not ok:
+            return
+        self.a._r = r
+        self.lineEdit_3.setText(str(r))
 
-    def input_dialog_get_S0_K(self, x: str):
-        num, ok = QInputDialog.getDouble(
-            self, "Окно ввода", "Введите значение " + x + ":", min=0.0
-        )
-        if ok:
-            return num
-        else:
-            raise Exception("Неверно введено " + x + "!")
+    def button_change_a(self):
+        a, ok = self.input_dialog_get_r_a_b("a")
+        if not ok:
+            return
+        self.a._a = a
+        self.lineEdit.setText(str(a))
 
-    def input_dialog_get_B0(self):
-        num, ok = QInputDialog.getDouble(
-            self, "Окно ввода", "Введите значение B0:", min=0.0
-        )
-        if ok and num > 0.0:
-            return num
-        else:
-            raise Exception("Неверно введено B0!")
+    def button_change_b(self):
+        b, ok = self.input_dialog_get_S0_K_b("b", -1.0)
+        if not ok:
+            return
+        self.a._b = b
+        self.lineEdit_2.setText(str(b))
 
-    def input_dialog_get_N(self):
-        num, ok = QInputDialog.getInt(
-            self, "Окно ввода", "Введите значение N:", min=0
-        )
-        if ok and num > 0:
-            return num
-        else:
-            raise Exception("Неверно введено N!")
+    def button_change_B0(self):
+        B0, ok = self.input_dialog_get_B0()
+        if not ok:
+            return
+        self.a._B0 = B0
+        self.lineEdit_4.setText(str(B0))
+
+    def button_change_S0(self):
+        S0, ok = self.input_dialog_get_S0_K_b("S0")
+        if not ok:
+            return
+        self.a._S0 = S0
+        self.lineEdit_5.setText(str(S0))
+
+    def button_change_K(self):
+        K, ok = self.input_dialog_get_S0_K_b("K")
+        if not ok:
+            return
+        self.a._K = K
+        self.lineEdit_6.setText(str(K))
+
+    def button_change_N(self):
+        N, ok = self.input_dialog_get_N()
+        if not ok:
+            return
+        self.a._N = N
+        self.lineEdit_7.setText(str(N))
+
+    def calculate(self):
+        if not self.a._a < self.a._r < self.a._b:
+            print("Должно быть a < r < b!")
+            return
+        self.main_window = Main_Form(self.a)
+        self.close()
+        self.main_window.show()
+
+
+class Main_Form(QMainWindow, mw.Ui_MainWindow):
+    def __init__(self, model_CRR):
+        super().__init__()
+        self.setupUi(self)
+        self.a = model_CRR
+        self.settext_lineedit_a_b_r_B0_S0_K_N_CN_K0()
+        self.update_beta_gamma()
+        self.stack_str = list()
+        self.stack_dict = dict()
+        self._is_connect_up_down = False
+        self.check_start()
+        self.pushButton_3.clicked.connect(self.get_choice_history)
+        self.pushButton_4.clicked.connect(self.button_calc_capital)
+        self.pushButton_5.clicked.connect(self.button_new_task)
+
+    def button_up(self):
+        a = self.a
+        self.push_state_to_stack()
+        a.increment_n()
+        a.calc_Sn_prev(a._b)
+        self.update_beta_gamma()
+        self.check_end()
+
+    def button_down(self):
+        a = self.a
+        self.push_state_to_stack()
+        a.increment_n()
+        a.calc_Sn_prev(a._a)
+        self.update_beta_gamma()
+        self.check_end()
+
+    def update_beta_gamma(self):
+        self.a.calc_gamma_n()
+        self.a.calc_beta_n()
+        self.update_n_beta_gamma_S_prev_line_edit()
+
+    def update_n_beta_gamma_S_prev_line_edit(self):
+        a = self.a
+        self.lineEdit_8.setText(str(round(float(a._Sn_prev), 4)))
+        self.lineEdit_11.setText(str(a._n - 1))
+        self.lineEdit_10.setText(str(round(a._beta_n, 4)))
+        self.lineEdit_9.setText(str(round(a._gamma_n, 4)))
+
+    def settext_lineedit_a_b_r_B0_S0_K_N_CN_K0(self):
+        a = self.a
+        self.lineEdit.setText(str(a._a))
+        self.lineEdit_2.setText(str(a._b))
+        self.lineEdit_3.setText(str(a._r))
+        self.lineEdit_4.setText(str(a._B0))
+        self.lineEdit_5.setText(str(a._S0))
+        self.lineEdit_6.setText(str(a._K))
+        self.lineEdit_7.setText(str(a._N))
+        self.lineEdit_8.setText(str(a._Sn_prev))
+        self.lineEdit_15.setText(str(round(float(a._CN), 4)))
+        self.lineEdit_16.setText(str(a._K0))
 
     def check_start(self):
-        a = self.a
-        if a._n < a._N and a._beta_n != 0.0 and a._gamma_n != 0.0 \
-            and (Decimal(a._beta_n) * a._B0 +
-                 Decimal(a._gamma_n) * a._S0) != Decimal(0.0):
+        if self.a._n <= self.a._N and \
+        self.a.calc_capital() != Decimal(0.0) and self.a._K0 <= self.a._N:
             self.button_up_down_connect()
 
     def check_end(self):
-        a = self.a
-        if a._n == a._N or a._beta_n == 0.0 and a._gamma_n == 0.0 \
-            or (Decimal(a._beta_n) * a._B0 +
-                Decimal(a._gamma_n) * a._S0) == Decimal(0.0):
+        if self.a._n > self.a._N or self.a.calc_capital() == Decimal(0.0):
             self.button_up_down_disconnect()
 
     def button_up_down_connect(self):
@@ -236,45 +326,39 @@ class Form(QMainWindow, mw.Ui_MainWindow):
             self, "Диалог возврата", "Выберите шаг:",
             self.stack_str, editable=False
         )
-        if ok and item:
-            item = self.stack_dict.get(item)
-            self.a._n = item[0]
-            self.a._beta_n = item[1]
-            self.a._gamma_n = item[2]
-            self.a._Sn_prev = item[3]
-            self.update_beta_gamma_line_edit()
+        if ok:
+            a = self.a
+            a._n, a._beta_n, a._gamma_n, a._Sn_prev = self.stack_dict.get(item)
+            self.update_n_beta_gamma_S_prev_line_edit()
             self.button_up_down_connect()
-            for i in range(self.a._n, len(self.stack_str)):
+            for i in range(a._n - 1, len(self.stack_str)):
                 self.stack_dict.pop(str(i))
                 self.stack_str.pop()
 
     def push_state_to_stack(self):
         a = self.a
-        save_str = str(a._n)
+        save_str = str(a._n - 1)
         self.stack_str.append(save_str)
         self.stack_dict[save_str] = [a._n, a._beta_n, a._gamma_n, a._Sn_prev]
 
-    def clear(self):
-        self.lineEdit.clear()
-        self.lineEdit_2.clear()
-        self.lineEdit_3.clear()
-        self.lineEdit_4.clear()
-        self.lineEdit_5.clear()
-        self.lineEdit_6.clear()
-        self.lineEdit_7.clear()
-        self.lineEdit_8.clear()
-        self.lineEdit_11.clear()
-        self.lineEdit_10.clear()
-        self.lineEdit_9.clear()
-        self.stack_dict.clear()
-        self.stack_str.clear()
-        self.button_up_down_disconnect()
+    def button_calc_capital(self):
+        QMessageBox.question(
+            self,
+            "Исполнение",
+            "Выплата равна: " + str(round(float(self.a.calc_capital()), 4)),
+            QMessageBox.Ok
+        )
+
+    def button_new_task(self):
+        self.start_window = Start_Form()
+        self.close()
+        self.start_window.show()
 
 
 def main():
     app = QApplication(sys.argv)
-    form = Form()
-    form.show()
+    start_form = Start_Form()
+    start_form.show()
     sys.exit(app.exec_())
 
 
